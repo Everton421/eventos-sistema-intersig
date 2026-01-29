04ee9d4e5ca1e1f68714f50363388d6fe0ce2de4
unknown <evertonjose421@gmail.com>
2026-01-29 15:51:37 -0300

    290120261551

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,29 @@ import { serviceVerifyClientEvent } from "./services/verify-event-clientes.ts";
 import { serviceVerifyRecebimentosEvent } from "./services/verify-event-recebimentos.ts";
 import { serviceVerifyPedidosEvent } from "./services/verify-event-pedidos.ts";
 
- connectRabbitMQ()
+const EMITIR_EVENTOS = process.env.EMITIR_EVENTOS as 'S' | 'N' ;
 
-  await serviceVerifyProdutctEvent();
- 
-  await serviceVerifyClientEvent()
+
+async function main(){
+
+ if(  EMITIR_EVENTOS == 'S'  ){
+  connectRabbitMQ()
+
+    await serviceVerifyProdutctEvent();
   
-  await serviceVerifyPedidosEvent()
+    await serviceVerifyClientEvent()
+    
+    await serviceVerifyPedidosEvent()
+
+    await serviceVerifyRecebimentosEvent()
+  
+  }else{
+    console.log("process.env.EMITIR_EVENTOS nao foi configurada!");
+ 
+ }
+ 
+}
+
  
 
-await serviceVerifyRecebimentosEvent()
- 
- 
+main();
