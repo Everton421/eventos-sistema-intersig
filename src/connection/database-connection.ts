96945f3fae4fc9a8438096f5355a961acd0ced5c
unknown <evertonjose421@gmail.com>
2026-03-23 15:29:39 -0300

    14:48 14:48 [ adicionado dockerfile]

--- src/connection/database-connection.ts
+++ src/connection/database-connection.ts
@@ -5,16 +5,13 @@ export const VENDAS = process.env.DB_VENDAS
 export const ESTOQUE = process.env.DB_ESTOQUE
 export const FINANCEIRO = process.env.DB_FINANCEIRO
 
-    
-const DB_CONFIG = { 
-                      host: process.env.DB_HOST,
+   export  const databaseEventos = `\`${process.env.DB_EVENTOS}\``;
+
+ const dbConn = await mysql.createPool( {
+     host: process.env.DB_HOST,
                       user:  process.env.DB_USER ,
                       password: process.env.DB_PASSWORD,
                       connectionLimit: 10
-                    };
-   export  const databaseEventos = `\`${process.env.DB_EVENTOS}\``;
-
-
- const dbConn = await mysql.createPool(DB_CONFIG);
+ });
      
 export default dbConn;
